7cf90f26e14e84b1bf6cfc5fd3f5b1363643931e
kian <kianrhyswells@gmail.com>
2021-12-12 15:56:19 +0000

    Added handling off both junit 4 and 5

---
+++ src/main/java/com/kxw959/programmingapplication/utils/JUNITRunner.java
@@ -0,0 +1,60 @@
+package com.kxw959.programmingapplication.utils;
+
+import com.kxw959.programmingapplication.HelloApplication;
+import com.kxw959.programmingapplication.user.User;
+import org.junit.internal.TextListener;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.TestPlan;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.junit.platform.launcher.listeners.TestExecutionSummary;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+
+import java.io.PrintWriter;
+
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
+
+public class JUNITRunner {
+    public String runJunit(String className){
+        ClassLoader loader = HelloApplication.class.getClassLoader();
+        try {
+            Class<?> testClass = Class.forName("com.kxw959.programmingapplication.tasks."+ className, true, loader);
+            if(User.JunitVersion == 4){
+                Result result = runJunit4(testClass);
+                return "Finished. Result: Failures: " +
+                        result.getFailureCount() + ". Ignored: " +
+                        result.getIgnoreCount() + ". Tests run: " +
+                        result.getRunCount() + ". Time: " +
+                        result.getRunTime() + "ms.";
+            }
+            else if(User.JunitVersion == 5){
+                TestExecutionSummary summary = runJunit5(testClass);
+                summary.printTo(new PrintWriter(System.out));
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public Result runJunit4(Class<?> junitClass){
+        JUnitCore junit = new JUnitCore();
+        return junit.run(junitClass);
+    }
+
+    public TestExecutionSummary runJunit5(Class<?> junitClass){
+        SummaryGeneratingListener listener = new SummaryGeneratingListener();
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+                .selectors(selectClass(junitClass))
+                .build();
+        Launcher launcher = LauncherFactory.create();
+        TestPlan testPlan = launcher.discover(request);
+        launcher.registerTestExecutionListeners(listener);
+        launcher.execute(request);
+        TestExecutionSummary summary = listener.getSummary();
+        return summary;
+    }
+}
